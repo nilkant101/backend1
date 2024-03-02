f30e9a661e7dd2043ae9b24041ee93124e71a77b
nilkant101 <nilwarle108@gmail.com>
2024-03-02 20:52:27 +0530

    dotenv

--- src/db/index.js
+++ src/db/index.js
@@ -6,11 +6,11 @@ const connectDB = async () => {
     try{
        const connectionInstance = await mongoose.connect(`${process.env.
         MONGODB_URI}/${DB_NAME}`)
-        console.log(`\MongoDB connected !! DB HOST:
+        console.log(`\n MongoDB connected !! DB HOST:
         ${connectionInstance.connection.host}`);
     } catch (error) {
-        console.log("MONGODB Connection error", error);
+        console.log("MONGODB Connection FAILD", error);
         process.exit(1)
     }
 }
-export default connectDB
+export default connectDB 
